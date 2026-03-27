eea8b8beb6a027cd10bf16eb18c5adcd1c248b08
Devika-Sajeesh <devikasajeesh26@gmail.com>
2026-03-27 10:57:54 +0530

    commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,24 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        String line = scanner.nextLine();
-        
+        	
+	String line = scanner.nextLine();
         String[] words = line.split("\\s+");
-        
-        Map<String, Integer> frequencyMap = new HashMap<>();
-        
+
+        LinkedHashMap<String, Integer> freqMap = new LinkedHashMap<>();
+
         for (String word : words) {
-            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
+            freqMap.put(word, freqMap.getOrDefault(word, 0) + 1);
         }
-        
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(frequencyMap.entrySet());
-        Collections.sort(list, Comparator.comparingInt((Map.Entry<String, Integer> entry) -> -entry.getValue())
-                .thenComparing((Map.Entry<String, Integer> entry) -> entry.getKey(), Comparator.reverseOrder()));
-        
-        for (Map.Entry<String, Integer> entry : list) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-        
+
+        for (Map.Entry<String, Integer> entry : freqMap.entrySet()) {
+               System.out.print(entry.getKey() + ": " + entry.getValue() + " ");      }
+
         scanner.close();
+
     }
 }
